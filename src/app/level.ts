594ec0d19a08ecdce10c53c51b37aab2fbe52164
lomserg <lomserg@gmail.com>
2024-04-24 21:17:11 +0300

    feat: add level class methods gethints

--- src/app/level.ts
+++ src/app/level.ts
@@ -3,16 +3,25 @@ export default class Level {
   size: number;
   valueString: string;
   row: string[][];
+  col: string[][];
+  topNums: number[][];
+  leftNums: number[][];
   won: boolean;
 
   constructor(name: string, size: number, valueString: string) {
     this.name = name;
     this.size = size;
     this.row = [];
+    this.col = [];
     this.valueString = valueString;
+    this.topNums = this.getHints(this.col);
+    this.leftNums = this.getHints(this.row);
     this.won = false;
     if (this.valueString) {
       this.row = this.rowVals();
+    }
+    if (this.valueString) {
+      this.col = this.colVals();
     }
   }
 
@@ -31,5 +40,44 @@ export default class Level {
       }
     }
     return rowsArrays;
+  }
+  colVals() {
+    let colsArrays = new Array(this.size);
+    for (let i = 0; i < colsArrays.length; i++) {
+      colsArrays[i] = [];
+    }
+    let i = 0;
+    while (i < this.valueString.length) {
+      let idx = i % this.size;
+
+      colsArrays[idx].push(this.valueString[i]);
+      i++;
+    }
+    return colsArrays;
+  }
+  getHints(vals: string[][]) {
+    let nums = [];
+    for (let i = 0; i < vals.length; i++) {
+      let temp = [];
+      let count = 0;
+      for (let j = 0; j < vals[i].length; j++) {
+        if (vals[i][j] === "0") {
+          if (count !== 0) {
+            temp.push(count);
+          }
+          count = 0;
+        }
+        if (vals[i][j] === "1") {
+          count++;
+        }
+      }
+      if (count !== 0) {
+        temp.push(count);
+      }
+      if (temp.length > 0) {
+        nums.push(temp);
+      }
+    }
+    return nums;
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
 import Level from "./app/level";
-const level1 = new Level(
-  "level_one",
-  10,
-  "0000111000001111111000100100101110010011111111111101111111100101111010001000010001110011100010000100"
-);
+const level1 = new Level("level_one", 3, "111100101");
 level1.rowVals();
 console.log(level1.rowVals());
+console.log(level1.colVals());
